eb23636eafc9282050dcb8c5d3805698fed42c0f
juliohsu <tnt700900@gmail.com>
2025-01-07 07:50:58 -0300

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ai-models
 
-A block of artificial intelligence models implemented with Pytorch.
+A block of artificial intelligence models self implemented with Pytorch.
 
 1 - Simple Linear Regression (y = x * w + b) <br>
 2 - Logistic Regression (y = x * w + b) + sigmoid layer <br>
--- rnn/model.py
+++ rnn/model.py
@@ -36,34 +36,39 @@ class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
-        self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
-    def forward(self, x, h0=None):
-        if h0 == None:
-            h0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
-        x, _ = self.rnn(x, h0)
-        x = self.fc(x[:, -1, :])
+        self.rnn = nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size * 2, output_size)
+        self.dropout = nn.Dropout(0.5)
+    def forward(self, x):
+        h0 = torch.zeros(2, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(2, x.size(0), self.hidden_size).to(x.device)
+        x, _ = self.rnn(x, (h0, c0))
+        x = self.dropout(x[:, -1, :])
+        x = self.fc(x)
         return x
 
 # configure custom rnn model
-model = RNN(12, 8, 2)
-optim = optimizer.Adam(model.parameters(), lr=0.001)
+model = RNN(12, 8, 2).to(device)
+optim = optimizer.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 criterion = nn.CrossEntropyLoss()
 
 # train
 for epoch in range(5):
+    model.train()
     epoch_losses = 0.0
     for loader_data, loader_labels in dataloader:
         loader_data = torch.nn.functional.one_hot(loader_data, num_classes=12).float()
         preds = model(loader_data)
         losses = criterion(preds, loader_labels)
-        epoch_losses += losses
+        epoch_losses += losses.item()
         optim.zero_grad()
         losses.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), max_norm=5)
         optim.step()
     print(f"epoch {epoch}, total losses: {epoch_losses:.4f}")
 
 # valid
+model.eval()
 with torch.no_grad():
     for zipper_data, zipper_label in zip(data, labels):
         zipper_data = torch.nn.functional.one_hot(zipper_data, num_classes=12).float()
